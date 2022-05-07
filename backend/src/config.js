92941a61310f1428b7536d9e9b3e17191d8a9dbe
Morazee <moraze_arm@got;ail.fr>
2022-05-06 19:29:17 -0700

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,7 +58,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x75dc147b69122AA594566fdE1E7D07bD08ca707C';
 const TREASURY_ADDRESS = '0x75dc147b69122AA594566fdE1E7D07bD08ca707C';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
