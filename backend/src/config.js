ff14371048f018f2169ed3144c734ab2119dc9de
Morazee <moraze_arm@got;ail.fr>
2022-05-06 20:08:31 -0700

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,7 +67,7 @@ const PUBLIC_MINT_START_DATE = "2022-07-11T11:30:48+00:00"; // This is required.
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-06-10T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "YOUR CONTRACT ADRESS "; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "YOUR CONTRACT ADRESS"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
